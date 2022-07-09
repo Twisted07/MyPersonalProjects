06d2dfffa9e37dbe3f26e703f9cf27af58a3176e
root <root@DESKTOP-64KEF0R.localdomain>
2022-07-09 14:47:39 +0100

    Simple Arithmetic Calculator

---
+++ PythonProjects/SimpleCalculator.py
@@ -0,0 +1,26 @@
+
+# A Basic Calculator
+'''
+This calculator performs basic arithmetic operations - addition, subtraction, multiplication and division
+Collects 3 input data - num1, num2 and operator
+Converts num1 and num2 to int
+Checks the type of operator inputed using if
+Prints the answer, else, "Incorrect input, Try again by inputting two numbers with a +, -, * or / operator."
+'''
+
+num1, operator, num2 = input("Enter your basic arithmetic below\n").split()
+num1 = int(num1)
+num2 = int(num2)
+
+if operator == "+":
+    print("{} + {} = {}".format(num1, num2, num1 + num2))
+elif operator == "-":
+    print("{} - {} = {}".format(num1, num2, num1 - num2))
+elif operator == "*":
+    print("{} * {} = {}".format(num1, num2, num1 * num2))
+elif operator == "/":
+    print("{} / {} = {}".format(num1, num2, num1 / num2))
+elif operator == "%":
+    print("{} % {} = {}".format(num1, num2, num1 % num2))
+else:
+    print("Incorrect input, Try again by inputting two numbers with a +, -, *, / or '%' operator.")
