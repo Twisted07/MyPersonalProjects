3c70b104d74260563fefe8b2b09b9635127b9549
Twisted07 <iamtwisted07@gmail.com>
2022-07-12 22:18:13 +0100

    Automated Electric Kettle

---
+++ C_Programming/electric_kettle.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+*This program automates the process of an electric kettle
+* by incrementing the volume of water 'vol' in the kettle to max volume 'maxVol'
+* then at maxVol and temp, T, less than 100 degrees, heating element increases in temp, heat,
+* till it reaches maxHeat. It remains constant until water temp reaches 100 degrees (T)
+* process ends
+* Return: 0 on success
+*/
+int main(void)
+{
+	int kettle;
+	int vol = 0;
+	int minVol = 0.20 * sizeof(kettle);
+	int maxVol = sizeof(kettle);
+	int T;
+	int heat;
+	int minT = 60;
+	int maxHeat = 110;
+	int maxT = 100;
+
+
+}
