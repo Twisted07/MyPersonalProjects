02a0b057967b6acac708fb877156c25c7a8f3417
Twisted07 <iamtwisted07@gmail.com>
2022-07-27 10:44:52 +0100

    Calculator updated

--- PythonProjects/ItemPriceCalculator.py
+++ PythonProjects/ItemPriceCalculator.py
@@ -1,7 +1,6 @@
 
 
 from asyncio.format_helpers import extract_stack
-from curses import tparm
 
 #Takes input of workers
 workers = input("How many workers does it take to produce this item? \n")
@@ -10,12 +9,20 @@ try:
 except:
     print("That is not a number")
 
+#Takes input of cost of materials
 materials = input("How much did you spend on materials and packaging? \n")
 try:
-    nmaterials: float(materials)
+    nmaterials = float(materials)
 except:
     print("Insert the amount it cost you to produce this item.\n")
+
+#Takes input of transportation fare
 transport = input("How much did you spend on transportation during production? \n")
+try:
+    ntransport = float(transport)
+except:
+    print("\nWrong input type. Please insert values in figures only.\n")
+
 
 #Takes input of workmanship
 workmanship = input("How much would you like to include as workmanship? \n")
@@ -24,14 +31,19 @@ try:
 except:
     print("Please insert an amount in digits. \n")
 
+#Takes input of miscellaneous costs 
 extras = input("How much would you like to include for extra costs? \n")
 try:
     nextras = float(extras)
 except:
     print("Please insert digits only. \n")
 
+#Takes input of profit
 profit = input("How much profit would you like to make on this item? \n")
 try:
     nprofit = float(profit)
 except:
     print("Insert profit in digits only.")
+
+sellingPrice = nprofit + nextras + (nworkmanship * nworkers) + nmaterials + ntransport
+print(sellingPrice)
