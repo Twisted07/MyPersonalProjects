32faa8b79a225a0a32fcaa551e35d6ab6538f79b
twisted07 <iamtwisted07@gmail.com>
2023-11-23 00:45:48 +0100

    Functionality: Completed dynamic loading of product section

--- portfolio/belovedsPlace/script.js
+++ portfolio/belovedsPlace/script.js
@@ -1,206 +1,144 @@
 const btnNodeList = document.querySelectorAll(".btn-show");
-const containerNodeList = document.querySelectorAll(".card__container");
-
-const categoryList = ["jewelry", "hair-accessories", "hair-making", "wig-making"];
-// const category = containerNodeList.map(node => node);
 const productSection = document.getElementById("products");
-console.log(productSection)
-// create section with className product
-// create h2 with className product__category
-// create div with className card__container
-
-const jewelryList = [
-    {
-        cardTitle: "Leather Earring",
-        cardImage: "url(../../img/product-images/leather-earring.jpeg)"
-    },
-    {
-        cardTitle: "Black Earring",
-        cardImage: "url(../../img/product-images/black-earring.jpeg)"
-    },
-    {
-        cardTitle: "Leather Love Earring",
-        cardImage: "url(../../img/product-images/leather-love-earring.jpeg)"
-    },
-    {
-        cardTitle: "Wooden Earring",
-        cardImage: "url(../../img/product-images/wooden-earrings.jpeg)"
-    },
-    {
-        cardTitle: "Rock Earring",
-        cardImage: "url(../../img/product-images/rocklike-earrings.jpeg)"
-    },
-    {
-        cardTitle: "Love Earring",
-        cardImage: "url(../../img/product-images/love-earrings.jpeg)"
-    },
-    {
-        cardTitle: "Butterfly Necklace",
-        cardImage: "url(../../img/product-images/butterfly-necklace.jpeg)"
-    },
-    {
-        cardTitle: "Infinity Necklace",
-        cardImage: "url(../../img/product-images/infinity-necklace.jpeg)"
-    },
-];
-
-const wigList = [
-    {
-        cardTitle: "Short Wig",
-        cardImage: "url(/img/product-images/short-wig.jpeg)"
-    },
-    {
-        cardTitle: "Dyed Curled Wig",
-        cardImage: "url(/img/product-images/dyed-curled-wig.jpeg)"
-    }
-];
-
-const hairMakingList = [
-    {
-        cardTitle: "Knotless Braids With Curls",
-        cardImage: "url(/img/product-images/knotless-braids-with-curls-1.jpeg)"
-    },
-    {
-        cardTitle: "Passion Twist",
-        cardImage: "url(/img/product-images/passion-twist.jpeg)"
-    },
-    {
-        cardTitle: "Bohemian Braid",
-        cardImage: "url(/img/product-images/Bohemian-braids.jpeg)"
-    }
-];
 
 
+// * This function takes a "product category list" and iterates over its content to gain access to their properties
+// * and use them to create the content for every product card that make up the card container at container "position" 
+// * in the nodeList.
 
-categoryList.forEach(category => {
-    product = document.createElement("section");
-    product.className = "product";
+function appendProductCard (productObject, node) {
+    productKey = productObject.productList
 
-    productCategory = document.createElement("h2");
-    productCategory.className = "product__category";
-    productCategory.textContent = category;
-    
-    cardContainer = document.createElement("div");
-    cardContainer.className = "card__container";
-
-    viewAllBtn = document.createElement("button");
-    viewAllBtn.className = "btn-show";
-    viewAllBtn.textContent = "Show all";
-    
-    product.appendChild(productCategory);
-    product.appendChild(cardContainer);
-    product.appendChild(viewAllBtn);
-    productSection.appendChild(product);
-})
-
-
-// The card content should be updated dynamically from the list 
-// todo: get the card container
-// todo: handle to card
-// todo: create <a> with classname card__image
-
-// todo: create div listName and an iterator that uniquely assigns className and background image property
-// todo: create card title
-// todo: feed in the card background image from the image list
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// containerNodeList.forEach(container => {
-//     if (container.childElementCount > 3) {
-//         const cards = container.children;
-//         for (count = 3; count < container.childElementCount; count++) {
-//             cards[count].style.display = "none";
-//         }
-//     }
-//     containerList.push(container);
-
-// })
-
-// for (let i = 0; i < btnNodeList.length; i++) {
-//     for (let button of btnNodeList) {
-//         button.onclick = () => {
-//             for (let content of containerNodeList[i]) {
-//                     console.log(content)
-//                 }
-//         }
-//     }
+    productKey.forEach(content => {
+        card = document.createElement("a");
+        card.href = "#";
+        card.className = "card";
         
-// }
-// console.log(containerList);
-// console.log(btnList);
+        productImage = document.createElement("div");
+        productImage.className = "card__image jewelry-1";
+        productImage.style.backgroundImage = content.cardImage;
+        
+        productName = document.createElement("h3");
+        productName.className = "card__title";
+        productName.textContent = content.cardTitle;
+        
+        card.appendChild(productImage);
+        card.appendChild(productName);
+        node.appendChild(card);
+
+    })
+
+}
+
+// * This function takes 
+function loadSection (data, parent, sectionName, sectionClass, categoryClass, buttonName, buttonClass) {
+
+    data.forEach(category => {
+        
+        sectionName = document.createElement("section");
+        sectionName.className = sectionClass;
+
+        sectionCategory = document.createElement("h2");
+        sectionCategory.className = categoryClass;
+        sectionCategory.textContent = category.categoryName;
+
+        cardContainer = document.createElement("div");
+        cardContainer.className = "card__container";
+
+        btn = document.createElement("button");
+        btn.className = buttonClass;
+        btn.textContent = buttonName;
+
+        sectionName.appendChild(sectionCategory);
+
+        appendProductCard(category, cardContainer);
+
+        sectionName.appendChild(cardContainer);
+        sectionName.appendChild(btn);
+        parent.appendChild(sectionName);
+    })
+
+}
 
 
+// ? We have to keep every product category object organized in a list so as to help keep track and make it easy to update
+productData = [
+    {
+        categoryName: "Jewelry",
+        productList: [
+            {
+                cardTitle: "Leather Earring",
+                cardImage: "url(../../img/product-images/leather-earring.jpeg)"
+            },
+            {
+                cardTitle: "Black Earring",
+                cardImage: "url(../../img/product-images/black-earring.jpeg)"
+            },
+            {
+                cardTitle: "Leather Love Earring",
+                cardImage: "url(../../img/product-images/leather-love-earring.jpeg)"
+            },
+            {
+                cardTitle: "Wooden Earring",
+                cardImage: "url(../../img/product-images/wooden-earrings.jpeg)"
+            },
+            {
+                cardTitle: "Rock Earring",
+                cardImage: "url(../../img/product-images/rocklike-earrings.jpeg)"
+            },
+            {
+                cardTitle: "Love Earring",
+                cardImage: "url(../../img/product-images/love-earrings.jpeg)"
+            },
+            {
+                cardTitle: "Butterfly Necklace",
+                cardImage: "url(../../img/product-images/butterfly-necklace.jpeg)"
+            },
+            {
+                cardTitle: "Infinity Necklace",
+                cardImage: "url(../../img/product-images/infinity-necklace.jpeg)"
+            },
+        ] 
+    },
+    {
+        categoryName: "Hair Accessories",
+        productList: [
+            {
+                cardTitle: "none",
+                cardImage: "none"
+            }
+        ]
+    },
+    {
+        categoryName: "Wigs",
+        productList: [
+            {
+                cardTitle: "Short Wig",
+                cardImage: "url(/img/product-images/short-wig.jpeg)"
+            },
+            {
+                cardTitle: "Dyed Curled Wig",
+                cardImage: "url(/img/product-images/dyed-curled-wig.jpeg)"
+            }
+        ]
+    },
+    {
+        categoryName: "Hair Making",
+        productList: [
+            {
+                cardTitle: "Knotless Braids With Curls",
+                cardImage: "url(/img/product-images/knotless-braids-with-curls-1.jpeg)"
+            },
+            {
+                cardTitle: "Passion Twist",
+                cardImage: "url(/img/product-images/passion-twist.jpeg)"
+            },
+            {
+                cardTitle: "Bohemian Braid",
+                cardImage: "url(/img/product-images/Bohemian-braids.jpeg)"
+            }
+        ]
+    }
+];
 
-
-
-
-
-
-// I want to view only one row at a time 
-// When button is clicked, the remaining cards should be displayed and the textcontent should change
-// i = 1;
-// viewBtn.forEach(btn => {
-//     console.log(btn)
-//     btn.className = `btn-show-${i}`;
-//     i++;
-//     btn.onclick = () => {
-
-//         cardContainer.item()
-//     cardContainer.forEach((container) => {
-//         cardTotal = container.childElementCount;
-//         cardList = container.children;
-//         if (cardTotal > 3) {
-//             console.log(cardTotal);
-            
-
-//         }
-//     });
-// }});
-
-// for (i = 0; i < cardContainer.length; i++) {
-
-//     viewBtn[i].onclick = () => {
-//         console.log(cardContainer[i])
-//         if (cardContainer[i].childElementCount > 3 && cardContainer[i].every(element => element.style === "block")) {
-//             for (j = 3; j < cardContainer[i].childElementCount; j++) {
-//                 element.style = "none";
-//             }
-//         }
-//         else {
-//             cardContainer[i].every(element => element.style = "block");
-//         }
-//     }
-// }
-
-// how about: foreach btn, we separate them into different handles, same for the
+loadSection(productData, productSection, "product", "product", "product__category", "showAll", "btn-show");
